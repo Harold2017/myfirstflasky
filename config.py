abd72431890dd41705034d90bd8f3e5be83c3f85
Harold <pxduharold@126.com>
2017-07-31 18:00:11 +0800

    change sqlite to mysql

--- config.py
+++ config.py
@@ -13,7 +13,7 @@ class Config:
     MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     FLASKY_MAIL_SUBJECT_PREFIX = 'Example'
     FLASKY_MAIL_SENDER = 'Example <example@gmail.com>'
-    FLASKY_ADMIN = os.environ.get('EXAMPLE_ADMIN')
+    FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
     FLASKY_POSTS_PER_PAGE = 20
     FLASKY_FOLLOWERS_PER_PAGE = 50
     FLASKY_COMMENTS_PER_PAGE = 30
@@ -26,18 +26,18 @@ class Config:
 class DevelopmentConfig(Config):
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
+                              ('mysql://flasky:123456@127.0.0.1/flasky')
 
 
 class TestingConfig(Config):
     TESTING = True
     SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'data-test.sqlite')
+                              ('mysql://flasky:123456@127.0.0.1/flasky')
 
 
 class ProductionConfig(Config):
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-        'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+                              ('mysql://flasky:123456@127.0.0.1/flasky')
 
 
 config = {
