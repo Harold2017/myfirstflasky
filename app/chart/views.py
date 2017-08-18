3d48c160ce9b33d5e0e57232b0e799460d793e01
Harold <pxduharold@126.com>
2017-08-18 10:17:44 +0800

    try multiple selection form

--- app/chart/views.py
+++ app/chart/views.py
@@ -25,10 +25,41 @@ def chart3():
     if Sensors.query.filter_by(author_id=current_user.id).first():
         sensors = Sensors.query.filter_by(author_id=current_user.id).order_by(Sensors.id.desc()).all()
         form = SelectMultipleSensorForm(sensors, prefix="sensorform")
-    if request.method == 'POST':
-        options = request.form.getlist('myform')
+        valid = 0
+
+        if form.validate_on_submit():
+            options = form.sensor.data
+            line = Line(title='LineChart', width=800, height=400)
+            for sensor in options:
+                sensor_data = Sensor_data.query.filter_by(sensor_id=sensor).order_by(-Sensor_data.id.desc()).all()
+                timestamp = []
+                data = []
+                for i in sensor_data:
+                    timestamp.append(i.timestamp.replace(tzinfo=utc).astimezone(tzchina).strftime('%Y/%m/%d-%H:%M:%S'))
+                    data.append(i.value)
+                if len(data) is 0:
+                    no_sensor = 0
+                    return render_template('no_sensor_dat.html', no_sensor=no_sensor)
+                else:
+                    s = Sensors.query.filter_by(id=sensor).first()
+                    title = s.name
+                    attr = timestamp
+                    d = data
+                    line.add(title, attr, d, is_smooth=False, is_datazoom_show=True, mark_line=["average"],
+                             mark_point=["min", "max"])
+            valid = 1
+            return render_template('sensor_chart.html', form=form, chart=line.render_embed(), valid=valid)
+        else:
+            valid = 0
+        return render_template('sensor_chart.html', form=form, valid=valid)
+    else:
+        no_sensor = 1
+        return render_template('no_sensor_dat.html', no_sensor=no_sensor)
+
+    #if request.method == 'POST':
+        #options = request.form.getlist('myform')
         #as_dict = request.form.to_dict()
         #print(request)
-        print(options)
+        #print(options)
 
-    return render_template('sensor_chart.html', form=form)
+
