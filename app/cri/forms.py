e7e187f95d98631159ea66ba2345d116a515cf39
Harold <pxduharold@126.com>
2017-08-21 11:45:10 +0800

    try multiple selection form

--- app/cri/forms.py
+++ app/cri/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import SelectField, SubmitField
+from wtforms import SelectField, SubmitField, SelectMultipleField
 from flask_wtf.file import FileField, FileAllowed
 from wtforms.validators import Required, Length
 from wtforms import ValidationError
@@ -22,10 +22,21 @@ class NoSpectrumForm(FlaskForm):
 
 
 class SelectSpectrumForm(FlaskForm):
-    spectrum = SelectField('Spectrum', coerce=int)
-    submit = SubmitField('Show spectrum')
+    spectra = SelectField('Sensors', coerce=int)
+    submit = SubmitField('Plot Data')
 
     def __init__(self, spectrum, *args, **kwargs):
         super(SelectSpectrumForm, self).__init__(*args, **kwargs)
-        self.s.choices = [(s.id, s.name) for s in spectrum]
+        self.spectra.choices = [(spectra.id, spectra.timestamp) for spectra in spectrum]
         self.spectrum = spectrum
+
+
+class SelectMultipleSpectrumForm(FlaskForm):
+    spectra = SelectMultipleField('Spectrum', coerce=int)
+    submit = SubmitField('Plot multi-spectrum')
+
+    def __init__(self, spectrum, *args, **kwargs):
+        super(SelectMultipleSpectrumForm, self).__init__(*args, **kwargs)
+        self.spectra.choices = [(spectra.id, spectra.timestamp) for spectra in spectrum]
+        self.spectrum = spectrum
+
--- app/cri/views.py
+++ app/cri/views.py
@@ -8,10 +8,13 @@ from pyecharts import Line
 import os
 import pandas as pd
 from flask_wtf import FlaskForm
-from wtforms import SelectField, SubmitField
+from wtforms import SelectField, SubmitField, SelectMultipleField
+from pytz import timezone
 #import math
 #import numpy as np
 
+tzchina = timezone('Asia/Shanghai')
+utc = timezone('UTC')
 
 upload_folder = os.path.abspath("app") + "\\uploads"
 extensions = set(['csv', 'txt', 'png', 'jpg', 'jpeg'])
@@ -23,6 +26,16 @@ class SelectSpectrumForm(FlaskForm):
 
     def __init__(self, spectrum, *args, **kwargs):
         super(SelectSpectrumForm, self).__init__(*args, **kwargs)
+        self.spectra.choices = [(spectra.id, spectra.timestamp) for spectra in spectrum]
+        self.spectrum = spectrum
+
+
+class SelectMultipleSpectrumForm(FlaskForm):
+    spectra = SelectMultipleField('Spectrum', coerce=int)
+    submit = SubmitField('Plot multi-spectrum')
+
+    def __init__(self, spectrum, *args, **kwargs):
+        super(SelectMultipleSpectrumForm, self).__init__(*args, **kwargs)
         self.spectra.choices = [(spectra.id, spectra.timestamp) for spectra in spectrum]
         self.spectrum = spectrum
 
@@ -90,18 +103,35 @@ def upload_spectrum():
 def cri_chart():
     if User_files.query.filter_by(author_id=current_user.id).first():
         spectrum = User_files.query.filter_by(author_id=current_user.id).order_by(User_files.id.desc()).all()
-        form = SelectSpectrumForm(spectrum)
+        form = SelectMultipleSpectrumForm(spectrum)
         chart = 0
     else:
         form = 0
         chart = 0
     if form.validate_on_submit():
-        spectra = form.spectra.data
-        user_file = User_files.query.filter_by(id=spectra).first()
-        id = user_file.id
-        chart = line_chart(id)
-        spectrum = User_files.query.filter_by(author_id=current_user.id).order_by(User_files.id.desc()).all()
-        form = SelectSpectrumForm(spectrum)
+        spectrum = form.spectra.data
+        line = Line(width=800, height=400)
+        for spectra in spectrum:
+            user_file = User_files.query.filter_by(id=spectra).first()
+            file_path = user_file.file_path
+            title = user_file.timestamp.replace(tzinfo=utc).astimezone(tzchina).strftime('%Y/%m/%d-%H:%M:%S')
+            if file_path is None:
+                flash('No Spectrum is uploaded!')
+                return render_template('404.html'), 404
+            with open(file_path) as f:
+                data = pd.read_csv(f, sep="\t" or ' ' or ',', header=None)
+                f.close()
+
+            if len(data) is 0:
+                flash('No data is uploaded!')
+
+            attr = [i[0] for i in data.values]
+            d = [i[1] for i in data.values]
+            line.add(title, x_axis=attr, y_axis=d, is_smooth=False, is_datazoom_show=True, mark_line=["average"],
+                     mark_point=["min", "max"])
+        chart = line.render_embed()
+    spectrum = User_files.query.filter_by(author_id=current_user.id).order_by(User_files.id.desc()).all()
+    form = SelectMultipleSpectrumForm(spectrum)
     return render_template('cri_chart.html', form=form, chart=chart)
 
 
