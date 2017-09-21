8633a5f32133b54a8ee88e99df66efe7ded6fd5c
Harold <pxduharold@126.com>
2017-09-21 13:25:19 +0800

    limit displaying number to 200 before adding data zoom function

--- app/mqtt/views.py
+++ app/mqtt/views.py
@@ -11,7 +11,7 @@ utc = timezone('UTC')
 
 @mqtt.route('/v1.0', methods=['GET'])
 def mqtt():
-    mqtt = mqtt_data.query.filter_by().order_by(mqtt_data.id.desc()).all()
+    mqtt = mqtt_data.query.filter_by().order_by(mqtt_data.id.desc()).limit(200).all()
     data = []
     for i in mqtt:
         tmp = {"timestamp": i.timestamp.replace(tzinfo=utc).astimezone(tzchina).strftime('%Y/%m/%d-%H:%M:%S')}
